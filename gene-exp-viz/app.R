65dc960175c7fcaab3027a6e037df3cfb1ea0b9c
H. Emre Etlioglu <emre.etlioglu@gmail.com>
2024-02-05 11:54:37 +0100

    version 1.0

--- gene-exp-viz/app.R
+++ gene-exp-viz/app.R
@@ -2,8 +2,12 @@ library(shiny)
 library(geneSynonym)
 library(ggplot2)
 
+source(
+  "https://raw.githubusercontent.com/etlioglu/bioinformatics/main/templates/customize-ggplot.r"
+)
+
 exp_meta_df <-
-  readRDS("deseq2-normalized-expression-for-tcgaovc-and-all-gtex.rds")
+  readRDS("deseq2-normalized-expression-for-ovc-colon-gbm-and-all-gtex.rds")
 genes_present <- colnames(exp_meta_df)[-1]
 
 check_gene <- function(x) {
@@ -21,6 +25,45 @@ check_gene <- function(x) {
   } else {
     return(x)
   }
+}
+
+filter_data_generate_plot <- function(data, gene, cancer) {
+  if (cancer == "ovarian") {
+    data <-
+      data[!(
+        data$TCGA_GTEX_main_category %in% c("Colorectal cancer", "Glioblastoma Multiforme")
+      ), ]
+  } else if (cancer == "colorectal") {
+    data <-
+      data[!(
+        data$TCGA_GTEX_main_category %in% c(
+          "Ovarian Serous Cystadenocarcinoma",
+          "Glioblastoma Multiforme"
+        )
+      ), ]
+  } else if (cancer == "gbm") {
+    data <-
+      data[!(
+        data$TCGA_GTEX_main_category %in% c("Ovarian Serous Cystadenocarcinoma", "Colorectal cancer")
+      ), ]
+  }
+  
+  ggplot(
+    data,
+    aes_string(x = "TCGA_GTEX_main_category",
+               y = gene,
+               color = "TCGA_GTEX_main_category")
+  ) +
+    geom_jitter(size = 0.5) +
+    theme(
+      axis.text.x = element_text(
+        angle = 90,
+        vjust = 0.5,
+        hjust = 1
+      ),
+      legend.position = "none"
+    ) +
+    coord_flip()
 }
 
 ui <- fluidPage(
@@ -44,36 +87,52 @@ ui <- fluidPage(
       width = 2
     ),
     
-    mainPanel(textOutput("gene"),
-              plotOutput("gene_exp_plot"),
-              width = 11)
+    
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("All cancers", plotOutput("gene_exp_plot_all")),
+        tabPanel("Ovarian Cancer", plotOutput("gene_exp_plot_ovarian")),
+        tabPanel("Colorectal Cancer", plotOutput("gene_exp_plot_colorectal")),
+        tabPanel("GBM", plotOutput("gene_exp_plot_gbm"))
+      )
+    )
   )
 )
 
 
 server <- function(input, output) {
+
+  output$gene_exp_plot_all <- renderPlot({
+    filter_data_generate_plot(
+      data = exp_meta_df,
+      gene = check_gene(input$gene_symbol),
+      cancer = "all"
+    )
+  })
   
-  # save check_gene(input$gene_symbol) in a variable in order not to call this multiple times
+  output$gene_exp_plot_ovarian <- renderPlot({
+    filter_data_generate_plot(
+      data = exp_meta_df,
+      gene = check_gene(input$gene_symbol),
+      cancer = "ovarian"
+    )
+  })
   
-  output$gene_exp_plot <- renderPlot({
-    ggplot(
-      exp_meta_df,
-      aes_string(
-        x = "TCGA_GTEX_main_category",
-        y = check_gene(input$gene_symbol),
-        color = "TCGA_GTEX_main_category"
-      )
-    ) +
-      geom_jitter(size = 0.5) +
-      theme(
-        axis.text.x = element_text(
-          angle = 90,
-          vjust = 0.5,
-          hjust = 1
-        ),
-        legend.position = "none"
-      ) +
-      coord_flip()
+  output$gene_exp_plot_colorectal <- renderPlot({
+    filter_data_generate_plot(
+      data = exp_meta_df,
+      gene = check_gene(input$gene_symbol),
+      cancer = "colorectal"
+    )
+  })
+  
+  output$gene_exp_plot_gbm <- renderPlot({
+    filter_data_generate_plot(
+      data = exp_meta_df,
+      gene = check_gene(input$gene_symbol),
+      cancer = "gbm"
+    )
   })
   
   output$to_download <-
